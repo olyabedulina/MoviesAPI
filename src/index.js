b4e4bbb6e0ce6b65f11e404584e5676dfeeeb08d
Olya Bedulina <olyabedulina@users.noreply.github.com>
2021-02-22 00:06:57 +0300

    MoviesAPI - Lesson 2 Webpack.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+import domReady from 'document-ready-promise'
+
+import './styles.css'
+import App from './components/App'
+
+const run = () => {
+    const appPlaceholder = document.createElement('div')
+    document.body.appendChild(appPlaceholder)
+
+    render(<App/>, appPlaceholder)
+}
+
+domReady().then(run)
