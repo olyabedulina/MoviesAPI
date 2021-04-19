638267aaba7fc55b66e6d2eb976119e54c610491
Olya Bedulina <olyabedulina@users.noreply.github.com>
2021-04-19 14:27:30 +0300

    MoviesAPI - Lesson 9 - Added tests for reducer

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -3,36 +3,36 @@ import renderer from 'react-test-renderer';
 
 import Button from './Button';
 
-describe('Button component test:', () => {
-    it('Testing "+ Add Movie" Button component', () => {
+describe('Components :: Button snapshots:', () => {
+    it('Test :: "+ Add Movie" Button component', () => {
         const actual = renderer
             .create(<Button kind="aux" className="buttonAddMovie">+ Add Movie</Button>)
             .toJSON();
         expect(actual).toMatchSnapshot();
     });
 
-    it('Testing "Search" Button component', () => {
+    it('Test :: "Search" Button component', () => {
         const actual = renderer
             .create(<Button type="submit" className="formButton">Search</Button>)
             .toJSON();
         expect(actual).toMatchSnapshot();
     });
 
-    it('Testing "Reset" Button component', () => {
+    it('Test :: "Reset" Button component', () => {
         const actual = renderer
             .create(<Button kind='alt' className="modalFooterButton">Reset</Button>)
             .toJSON();
         expect(actual).toMatchSnapshot();
     });
 
-    it('Testing "Submit" Button component', () => {
+    it('Test :: "Submit" Button component', () => {
         const actual = renderer
             .create(<Button type="submit" kind='main' className="modalFooterButton">Submit</Button>)
             .toJSON();
         expect(actual).toMatchSnapshot();
     });
 
-    it('Testing "Go Back To Home" Button component', () => {
+    it('Test :: "Go Back To Home" Button component', () => {
         const actual = renderer
             .create(<Button kind='alt' className="pageNotFoundButton">Go Back To Home</Button>)
             .toJSON();
